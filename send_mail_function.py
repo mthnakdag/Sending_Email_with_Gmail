c2011c0bbc67804d2503d3088b1a4fd42ec67e40
Metehan Akdag <35811666+mthnakdag@users.noreply.github.com>
2020-05-02 16:40:35 +0300

    Update send_mail_function.py

--- send_mail_function.py
+++ send_mail_function.py
@@ -1,38 +1,44 @@
-import smtplib
-import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-username = "akdag.metehan231345@gmail.com"
-password = "asdzxd12d"
-
-
-def send_mail(text="Email Body", subject="Hello",
-              from_email="Metehan Akdag <akdag.metehan231345@gmail.com>",
-              to_emails=None, html=None, img_path=None):
-    to_emails = "akdag.metehan231345@gmail.com"
-    img_path = "python.jpg"
+def send_mail(text=None, subject="",
+              from_email=None, to_email=None,
+              html = None,img_path=None):
+    
+    """
+    text is input email body -- str
+    subject is input email subject -- str
+    from_email which mail addresses -- str
+    to_email send email who . You can change some code parts to_email convert a list with (", ").join format --str
+    html if your body part is not plain text hyper text also you can send html --str
+    img_path if you can adding a image give image path -- str
+    """
+    
+    assert isinstance(text,str) or isinstance(html,str)
     assert isinstance(from_email, str)
-    assert isinstance(to_emails, str)
-    msg = MIMEMultipart("alternative")
-    msg["From"] = from_email
-    msg["To"] = to_emails
+    assert isinstance(to_email, str)
+    
+    msg = MIMEMultipart("alternative")# Creating a msg object suitable for smtp format
+    msg["From"] = from_email 
+    msg["To"] = to_email
     msg["Subject"] = subject
-    txt_part = MIMEText(text, "plain")
-    msg.attach(txt_part)
-    if img_path is not None:
-        image = MIMEImage(open(img_path, "rb").read(), name= os.path.basename(img_path))
-        msg.attach(image)
-    if html is not None:
-        html_part = MIMEText(html, "html")
-        msg.attach(html_part)
+    
+
+    if text is not None:                 # if text coming
+        txt_part = MIMEText(text, "plain")
+        msg.attach(txt_part) #attaching body
         
-    msg_str = msg.as_string()
-    #server = smtplib.SMTP()
-    with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
+    if html is not None:                 # if html coming
+        html_part = MIMEText(html,"html")
+        msg.attach(html_part)            # attaching body
+        
+    if img_path is not None:             # if image coming
+        image = MIMEImage(open(img_path, "rb").read(), name= os.path.basename(img_path))
+        msg.attach(image)                # attaching image
+        
+    msg_str = msg.as_string() # msg convert the string format
+    
+    
+    with smtplib.SMTP(host= HOST, port= PORT) as server: #server communication
         server.ehlo()
         server.starttls()
-        server.login(username, password)
-        server.sendmail(from_email, to_emails, msg_str)
-    #server.quit()
+        server.login(USERNAME, PASSWORD)
+        server.sendmail(from_email, to_email, msg_str)
 
